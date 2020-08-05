451119df92a229f46faa4656e101afae7f2a2aa8
ajiiz <opticpiter@gmail.com>
2020-08-05 21:41:31 +0200

    Added fetching data method with error handling

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
     const [quote, setQuote] = useState('there goes quote')
     const [author, setAuthor] = useState('there goes author')
+    const [error, setError] = useState({})
+    const [loading, setLoading] = useState(false)
+
+    const handleClick = () => {
+        let ignore = false
+        const fetchData = async() => {
+            try {
+                setLoading(true)
+                setError({})
+                const response = await axios('https://api.quotable.io/random')
+                if(!ignore) {
+                    setAuthor(response.data.author)
+                    setQuote(response.data.content)
+                }
+            } catch(err) {
+                setError(false)
+            }
+            setLoading(false)
+        }
+        fetchData()
+        return(() => {
+            ignore = true
+        })
+    }
+
     return (
         <div className="main">
-            <h1>{quote}</h1>
-            <p>{author}</p>
+
         </div>
     )
 }
