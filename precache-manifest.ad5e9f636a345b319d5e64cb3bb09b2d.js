d1154ab149add54ac94531b0d4b8236387b52d0b
ajiiz <opticpiter@gmail.com>
2020-08-08 15:03:30 +0200

    Updates

---
+++ precache-manifest.ad5e9f636a345b319d5e64cb3bb09b2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98b89ef6fbd160d8ac9fe6bd2b07e2fd",
+    "url": "/random-quote-react-app/index.html"
+  },
+  {
+    "revision": "f76e2dce346957f9d055",
+    "url": "/random-quote-react-app/static/css/main.f4de18ca.chunk.css"
+  },
+  {
+    "revision": "a54dde8015fc415eaeaf",
+    "url": "/random-quote-react-app/static/js/2.6b8ab440.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/random-quote-react-app/static/js/2.6b8ab440.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f76e2dce346957f9d055",
+    "url": "/random-quote-react-app/static/js/main.85584b64.chunk.js"
+  },
+  {
+    "revision": "edae5c59ec7d1bcb7798",
+    "url": "/random-quote-react-app/static/js/runtime-main.c91607ec.js"
+  }
+]);
